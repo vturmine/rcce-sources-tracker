acc307ca32402e477f078b6d7531461fbd705989
amadu <ndongamadou@gmail.com>
2022-01-05 16:40:42 +0100

    disclaimer

--- src/js/ustats.js
+++ src/js/ustats.js
@@ -121,9 +121,11 @@ function drawPanelChart(name){
     $('#overview').html('');
     $('#overview')
         .append(
-            '<div class="col-md-6 key-figure">'+
-                '<span class="num" id="totalSources">'+countryData_.length+'</span>'+
-                '<span id="sourceLabel"> QUANTITATIVE studies</span>'+
+            '<div class="row">'+
+                '<div class="col-md-12 key-figure">'+
+                    '<span class="num" id="totalSources">'+countryData_.length+'</span>'+
+                    '<span id="sourceLabel"> QUANTITATIVE studies</span>'+
+                '</div>'+
             '</div>'+
             '<div class="row">'+
                 '<div class="col-md-12">'+
