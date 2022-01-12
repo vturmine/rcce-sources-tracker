dc3647fbcf47b96ab7778c9108292e1612ad88cb
amadu <ndongamadou@gmail.com>
2022-01-12 15:09:08 +0100

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -65,6 +65,7 @@ function getColumnUniqueValues(){
 // map js
 let countriesArr = [];
 let g, mapsvg, projection, width, height, zoom, path;
+let viewportWidth = window.innerWidth;
 let currentZoom = 1;
 let mapClicked = false;
 let selectedCountryFromMap = "all";
@@ -76,7 +77,7 @@ let mapFillColor = '#204669',//'#C2DACA',//'#2F9C67',
 let countryIso3Code = 'ISO_A3',
     countryGeoName = 'NAME';
 function initiateMap() {
-    width = $('#map').width();
+    width = viewportWidth;
     height = 500;
     var mapScale = width/10.6;
     var mapCenter = [25, 25];
@@ -743,7 +744,7 @@ function drawPanelChart(name){
     $('#globalStats').addClass('hidden');
     $('#overview').removeClass('hidden');
 }//drawPanelChart
-let geodataUrl = 'data/worldmap.json';
+let geodataUrl = 'data/wld.json';
 let data_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQwVG8xzJogJgeBzk5-mLaA7BOGWhwU_Z6iGrnGQwPT2OAInzFYX-5hNYh2aFyqn0sVh0PpFikSJuEq/pub?gid=1612863274&single=true&output=csv';
 // 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRQDTIju76FYADX9ZKbKBD1JBA7eFLv86Y8ltOTs24eLqrf3FnKJENmtcUkP1HUMCQq7JL9hgwofz0q/pub?gid=1612863274&single=true&output=csv';
 
@@ -757,7 +758,7 @@ $( document ).ready(function(){
             d3.json(geodataUrl),
             d3.csv(data_url),
         ]).then(function(data){
-            geomData = topojson.feature(data[0], data[0].objects.geom);
+            geomData = topojson.feature(data[0], data[0].objects.worldtopo12022020);
             sourcesData = data[1];
 
             sourcesDataFiltered = data[1];
@@ -765,6 +766,7 @@ $( document ).ready(function(){
             countriesArr = arrs[0],
             dimensionsArr = arrs[1],
             regionsArr.push(...arrs[2]);
+            console.log(geomData)
             generateRegionDropdown();
             // init map global stats
             initiateMap();
